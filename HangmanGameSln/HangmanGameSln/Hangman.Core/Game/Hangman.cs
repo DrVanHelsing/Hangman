159e192d9a8bcceede158b2f570cae7064d6934e
Tredir <tredirs@gmail.com>
2025-05-13 10:49:26 +0200

    added a visual word list
    the word list updates showing only the available letters left to use

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using HangmanRenderer.Renderer;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -18,6 +19,8 @@ namespace Hangman.Core.Game
 
         private string[] word_list = { "help", "build", "computer" };
 
+        private char[] letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
         private Random rdm_num = new Random();
 
         private char[] correct_word;
@@ -28,19 +31,18 @@ namespace Hangman.Core.Game
 
         private List<char> user_guesses = new List<char>();
 
-        private int wrong_guess = 6;
+        private int num_lives = 6;
 
         private string get_word()
         {
             return word_list[rdm_num.Next(word_list.Length)];
-
         }
 
         private void get_guess()
         {
             Console.SetCursorPosition(0, 15);
             Console.Write("What is your next guess: ");
-            nextGuess = Convert.ToChar(Console.ReadLine());
+            nextGuess = Convert.ToChar(Console.ReadLine().ToLower());
         }
 
         private bool check_guess()
@@ -50,7 +52,6 @@ namespace Hangman.Core.Game
                 if (nextGuess == correct_word[i])
                 {
                     user_guesses[i] = nextGuess;
-
                     return true;
                 }
             }
@@ -85,11 +86,68 @@ namespace Hangman.Core.Game
             }
         }
 
+        private string x_box(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            return "----------------------------------";
+        }
+
+        private string y_box(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            return "|";
+        }
+
+        private void update_letter_list() 
+        {
+            Console.Write(x_box(50,5));
+            Console.Write(x_box(50, 17));
+
+            Console.SetCursorPosition(60,7);
+            Console.Write("Unused Letters:");
+
+            Console.SetCursorPosition(53,9);
+            for (int letter_num = 0; letter_num < 10; letter_num++)
+            {
+                
+                Console.Write(letters[letter_num] +"  ");
+            }
+            Console.SetCursorPosition(53, 12);
+            for (int letter_num = 10; letter_num < 20; letter_num++)
+            {
+
+                Console.Write(letters[letter_num] + "  ");
+            }
+            Console.SetCursorPosition(53, 15);
+            for (int letter_num = 20; letter_num < 26; letter_num++)
+            {
+
+                Console.Write(letters[letter_num] + "  ");
+            }
+
+            for (int i = 0; i < 13; i++)
+            {
+                Console.Write(y_box(51, i +5));
+                Console.Write(y_box(82, i + 5));
+            }
+
+            for (int alphabet = 0; alphabet < letters.Length; alphabet++)
+            {
+                if (letters[alphabet] == nextGuess)
+                {
+                    letters[alphabet] = ' '; 
+                }
+            }
+        }
+
         public void Run()
         {
             _renderer.Render(5, 5, 6);
 
             correct_word = get_word().ToCharArray();
+
+            update_letter_list();
+            
 
             user_guesses.Clear();
             for (int i = 0; i < correct_word.Length; i++)
@@ -104,24 +162,26 @@ namespace Hangman.Core.Game
 
             while (true)
             {
+                update_letter_list();
                 get_guess();
+                update_letter_list();
 
                 if (check_guess())
                 {
                     update_word_status();
                     Console.SetCursorPosition(0, 17);
-                    Console.WriteLine("Nice!");
+                    Console.WriteLine("Nice! \n");
                 }
                 else
                 {
-                    wrong_guess--;
+                    num_lives--;
                     Console.SetCursorPosition(0, 18);
-                    Console.WriteLine($"Oopsie! {wrong_guess} guesses remaining, try again...");
-                    _renderer.Render(5, 5, wrong_guess);
+                    Console.WriteLine($"Oopsie! {num_lives} guesses remaining, try again...");
+                    _renderer.Render(5, 5, num_lives);
                     
-                    if (wrong_guess == 0)
+                    if (num_lives == 0)
                     {
-                        _renderer.Render(5, 5, wrong_guess);
+                        _renderer.Render(5, 5, num_lives);
                         Console.SetCursorPosition(5, 19);
                         Console.WriteLine($"You Died! \nThe Word is {new string(correct_word)}");
                         break;
@@ -140,3 +200,11 @@ namespace Hangman.Core.Game
 
     }
 }
+
+/*
+ *TO DO:
+ *fix functionality bug: Allow for input of words with repeated letters
+ *
+ *DONE:
+ *display letter list of previously inputted letters --> remove used letters from list
+*/ 
